fd781e6e90b887e8fb949109f92864e1e98a5d27
Sasha <none>
2019-05-09 18:22:10 +0300

    Fixed properties of Point2D class

--- Point2D.py
+++ Point2D.py
@@ -2,22 +2,31 @@ import math
 
 
 class Point2D:
-
     def __init__(self, id_initial, x_initial, y_initial):
         self.id = id_initial
         self.x = x_initial
         self.y = y_initial
 
     @property
-    def x(self):
-        return self.x
+    def id(self):
+        return self._id
 
-    def y(self):
-        return self.y
+    @id.setter
+    def id(self, value):
+        self._id = value
+
+    @property
+    def x(self):
+        return self._x
 
     @x.setter
-    def x(self, x):
-        self.x = x
+    def x(self, value):
+        self._x = value
 
-    def y(self, y):
-        self.y = y
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, value):
+        self._y = value
--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import Point2D as P
+import Point2D as Pnt
 
 
 if __name__ == '__main__':
-
-    p1 = P.Point2D(1234, 1, 1)
-    p1.x
-    
